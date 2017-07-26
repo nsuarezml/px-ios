2182b331e7b5ff8546cbccc20ec38433d2022da1
Augusto Collerone <collerone.augusto@gmail.com>
2017-07-26 13:43:42 -0300

    LocalizableStringUtil file CREATED

---
+++ MercadoPagoSDK/MercadoPagoSDK/LocalizableStringsUtil.swift
@@ -0,0 +1,28 @@
+//
+//  LocalizableStringsUtil.swift
+//  MercadoPagoSDK
+//
+//  Created by AUGUSTO COLLERONE ALFONSO on 7/26/17.
+//  Copyright © 2017 MercadoPago. All rights reserved.
+//
+
+import Foundation
+
+public class LocalizableStringsUtil: NSObject {
+    
+    //Basic UI
+    open static let SI_STRING = "Si".localized
+    open static let NO_STRING = "No".localized
+    open static let CARGANDO_STRING = "Cargando...".localized
+    open static let ERROR_STRING = "Hubo un error".localized
+    open static let CONTINUAR_STRING = "Continuar".localized
+    open static let SALIR_STRING = "Salir".localized
+    open static let REINTENTAR_STRING = "Reintentar".localized
+    
+    
+    
+    open static let CODIGO_DE_DESCUENTO_STRING = "Código de descuento".localized
+    
+    
+
+}
