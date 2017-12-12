621586623e0e69f9c683ab778290dd72a207fbd9
Eden Torres <eden.etorres@gmail.com>
2017-12-12 15:21:24 -0300

    Add optinal hook

---
+++ PodTester/PodTester/Payment Method Plugins/PaymentMethodPluginConfigViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PaymentMethodPluginConfigViewController.swift
+//  PodTester
+//
+//  Created by Eden Torres on 12/12/17.
+//  Copyright © 2017 AUGUSTO COLLERONE ALFONSO. All rights reserved.
+//
+
+import UIKit
+
+class PaymentMethodPluginConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
